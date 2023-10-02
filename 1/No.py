5a50279c2f6e157ffd8d8e47b92b36a53df088cf
Jonas D'Angelo <jonas.moreira@viavarejo.com.br>
2023-10-02 19:40:32 -0300

    .

---
+++ 1/No.py
@@ -0,0 +1,5 @@
+class No:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.right = right
+        self.left = left
---
+++ 1/OrdemArvore.py
@@ -0,0 +1,8 @@
+from Arvore import Arvore
+
+list = [5,2,4,12]
+
+binaryTree = Arvore()
+binaryTree.createBinaryTreeByList(valueList=list)
+
+print(binaryTree)
