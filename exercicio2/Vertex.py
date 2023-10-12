9c9da21ba0ebcb12650e0d2dd657073a665f1f92
Jonas D'Angelo <jonasgm.dangelo@gmail.com>
2023-10-12 14:45:31 -0300

    .

--- exercicio2/Vertex.py
+++ exercicio2/Vertex.py
@@ -3,13 +3,3 @@ class Vertex:
     def __init__(self, key=None):
         self.key = key
         self.edgeList = []
-        self.getCheck = False
-    
-    def createEdgeList(graphDict):
-        i = 0
-        for key in self.getGraphDict:
-            x = 0
-            for valueArr in self.getGraphDict[key]:
-                self.getVertex[i].edgeList.append(Edge(value=valueArr[1],vertexA=key,vertexB=[0]))
-                x+=1
-            i+=1
